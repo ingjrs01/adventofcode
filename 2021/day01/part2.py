25fab4a356af24bdbad5598b91fa595acc05504f
Juan Ramón Riveiro <juanriveiro@gmail.com>
2021-12-01 12:34:09 +0000

    feat: second part

--- 2021/day01/part2.py
+++ 2021/day01/part2.py
@@ -1,20 +1,15 @@
-print("Advent of code 2021")
-
-f = open('input2.txt',"r")
-
+data = [int (a.strip()) for a in open('input','r').readlines()]
 cont = 0
-data = [int (a.strip()) for a in f.readlines()]
 
-anterior = data[0] + data[1] + data[2]
+previous = data[0] + data[1] + data[2]
 
-tamp = len(data)
-print(tamp)
-for i in range(1,tamp):
-    if (i + 2 < tamp):
+size = len(data)
+for i in range(1,size):
+    if (i + 2 < size):
         val = data[i] + data[i+1] + data[i+2]
-        if (val > anterior):
+        if (val > previous):
             cont += 1
 
-        anterior = val
+        previous = val
 
 print(cont)
